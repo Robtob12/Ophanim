246578fd9e1c4481b50bdc00d6f7faf6343b060f
Robtob12 <robtobgamer12@gmail.com>
2026-03-29 13:31:31 -0300

    primera estructura de la pagina y idea principal del proyecto

---
+++ public/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ophanim DDB</title>
+    <link rel="stylesheet" href="styles/index.css">
+</head>
+<body>
+    <nav>
+        <a href="index.php">
+            <img src="image/icon.png" alt="" class="icon">
+        </a>
+
+        <div class="links">
+            <a href="">Documentacion</a>
+            <a href="">Herramientas</a>
+            <a href="">Creador</a>
+            <a href="">Ajuda</a>
+            <div class="login">
+                <a href="#">Entrar</a>
+            </div>
+        </div>
+    </nav>
+    <header>
+        <div class="box1">
+            <h1>Una Herramienta que facilita el trabajo para todos</h1>
+            <p>
+                Ophanim es un na herramienta de banco de datos dinamica 
+                que ajuda a estructurar, conectar y gerenciar tu proprio banco de datos
+                desde la web. 
+            </p>
+
+            <div class="divisor">
+                <a class="btn1" href=""><span class="hoverLink"> < </span> Provar ya! <span class="hoverLink"> > </span></a>
+                <a class="btn2" href=""><span class="hoverLink"> < </span> Hacer login  <span class="hoverLink"> > </span></a>
+            </div>
+        </div>
+        <div class="box2">
+            <img src="image/database.jpg" alt="">
+        </div>
+    </header>
+
+</body>
+</html>
